8a6cd25029f1d847cf84fcec12c84390dd434d4a
Jun Li <junli@fb.com>
2019-11-18 01:26:22 -0800

    create dummy YodaMecanumDrive class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/mecanum/YodaMecanumDrive.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.drive.mecanum;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREVOptimized;
+
+public class YodaMecanumDrive extends SampleMecanumDriveREVOptimized {
+
+    public YodaMecanumDrive(HardwareMap hardwareMap) {
+        super(hardwareMap);
+    }
+}
