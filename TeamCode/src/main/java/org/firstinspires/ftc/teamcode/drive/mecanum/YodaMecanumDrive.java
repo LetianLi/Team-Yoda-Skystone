534dfe3fe1c2489f997378c838879f06acbc6e49
samsonli <samsonlijun@hotmail.com>
2019-11-23 23:29:34 -0800

    move files to teamcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/mecanum/YodaMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/mecanum/YodaMecanumDrive.java
@@ -9,8 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREVOptimized;
-import org.firstinspires.ftc.teamcode.util.SKYSTONEVuforiaDetector;
+import org.firstinspires.ftc.teamcode.teamcode.SKYSTONEVuforiaDetector;
 import org.openftc.revextensions2.ExpansionHubEx;
 import org.openftc.revextensions2.ExpansionHubMotor;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/SkystonePos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/SkystonePos.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode.teamcode;
 
 public enum SkystonePos {
     LEFT,
